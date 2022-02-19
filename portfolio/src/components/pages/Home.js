bca833028df49ab13e0427e2365eb57ffa0fe3ab
Matthew Cross <matthewcross1017@gmail.com>
2022-02-18 21:30:28 -0500

    Fix nav bar add pages add content

--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function About() {
+export default function Home() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>Home Page</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
         velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
